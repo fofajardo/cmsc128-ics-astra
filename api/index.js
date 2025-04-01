8385cfa391c0292ed3cc3a41b3b8373a73667320
jdvillamin <jdvillamin@up.edu.ph>
2025-04-01 14:14:45 +0800

    feat: initial API testing set-up

--- api/index.js
+++ api/index.js
@@ -1,16 +1,19 @@
-/*
- * Base imports.
- */
 import express from "express";
+import httpStatus from "http-status-codes";
 
 const kPort = 3001;
-
 const gServer = express();
 
 // Use appropriate parsers to access the request/response body directly.
 gServer.use(express.json());
 gServer.use(express.urlencoded({ extended: false }));
 
-gServer.listen(kPort, function() {
+gServer.get("/", (req, res) => {
+    res.status(httpStatus.OK).json({ message: "API is working!" });
+});
+
+export default gServer;
+
+gServer.listen(kPort, () => {
     console.log(`Listening to port: ${kPort}`);
 });
