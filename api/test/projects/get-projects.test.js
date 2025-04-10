99fb10151d891f49ae9b4cbc02859cb05fdee882
Nico De Castro <jtdecastro@up.edu.ph>
2025-04-10 14:34:37 +0800

    test: add get projects tests

---
+++ api/test/projects/get-projects.test.js
@@ -0,0 +1,48 @@
+import request from 'supertest';
+import { expect } from 'chai';
+import app from '../../index.js';
+import httpStatus from 'http-status-codes';
+
+describe('Projects API Tests', function () {
+
+    describe('GET /v1/projects', function () {
+        it('should return 200 and a list of projects', async function () {
+            const res = await request(app)
+                .get('/v1/projects')
+                .query({ page: 1, limit: 10 });
+
+            expect(res.status).to.equal(httpStatus.OK);
+            expect(res.body).to.be.an('object');
+            expect(res.body).to.have.property('status', 'OK');
+            expect(res.body).to.have.property('list').that.is.an('array');
+        });
+    });
+
+    describe('GET /v1/projects/:projectId', function () {
+        it('should return 200 and details of a single project', async function () {
+            const projectId = '7f857ca0-fcca-4c5b-b619-d0612597dbb1'; // Actual project_id
+            const res = await request(app).get(`/v1/projects/${projectId}`);
+
+            expect(res.status).to.equal(httpStatus.OK);
+            expect(res.body).to.be.an('object');
+
+            expect(res.body).to.have.property('status', 'OK');
+            expect(res.body).to.have.property('project').to.be.an('object');
+
+            const projectData = res.body.project;
+
+            expect(projectData).to.have.property('project_id');
+
+            expect(projectData).to.have.property('status').that.is.oneOf([0, 1, 2]);
+
+            expect(projectData).to.have.property('due_date');
+            expect(new Date(projectData.birthdate).toString()).to.not.equal('Invalid Date');
+
+            expect(projectData).to.have.property('date_completed');
+
+            expect(projectData).to.have.property('goal_amount').that.is.a('float');
+
+            expect(projectData).to.have.property('donation_link').that.is.a('string');
+        });
+    });
+});
