c8589b3e0b9e448e9a2db8d5eac5f4e6557ea665
Nico De Castro <jtdecastro@up.edu.ph>
2025-04-10 16:51:38 +0800

    feat: implement get projects route

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import getUsersRouter from "./routes/users/get-users.js";
 import getAlumniRouter from "./routes/alumni/get-alumni.js";
 import getJobsRouter from "./routes/jobs/get-jobs.js";
 import getEventsRouter from "./routes/events/get-events.js";
+import getProjectsRouter from "./routes/projects/get-projects.js";
 
 env.config();
 
@@ -25,6 +26,7 @@ gServer.use('/v1/users', getUsersRouter(testingSupabase));
 gServer.use('/v1/alumni', getAlumniRouter(testingSupabase));
 gServer.use('/v1/jobs', getJobsRouter(testingSupabase));
 gServer.use('/v1/events', getEventsRouter(testingSupabase));
+gServer.use('/v1/projects', getProjectsRouter(testingSupabase));
 
 export default gServer;
 
---
+++ api/routes/projects/get-projects.js
@@ -0,0 +1,79 @@
+import express from 'express';
+import httpStatus from 'http-status-codes';
+
+const getProjectsRouter = (supabase) => {
+    const router = express.Router();
+
+    router.get("/", async (req, res) => {
+        try {
+            const { page = 1, limit = 10 } = req.query;
+            const startIndex = (page - 1) * limit;
+            const endIndex = startIndex + Number(limit) - 1;
+
+            const { data, error } = await supabase
+                .from("projects")
+                .select()
+                .range(startIndex, endIndex);
+
+            if (error) {
+                return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+                    status: "FAILED",
+                    message: error.message
+                });
+            }
+
+            // console.log(data);
+
+            return res.status(httpStatus.OK).json({
+                status: "OK",
+                list: data || [],
+            });
+
+        } catch (error) {
+            return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+                status: "FAILED",
+                message: error.message
+            });
+        }
+    });
+
+    router.get("/:projectId", async (req, res) => {
+        try {
+            const { projectId } = req.params;
+
+            const { data, error } = await supabase
+                .from("projects")
+                .select(
+                    "project_id, status, due_date, date_completed, goal_amount, donation_link"
+                )
+                .eq("project_id", projectId)
+                .single();
+
+            console.log(data);
+
+            if (error) {
+                return res.status(httpStatus.NOT_FOUND).json({
+                    status: "FAILED",
+                    message: "Project not found"
+                });
+            }
+
+            // console.log(data);
+
+            return res.status(httpStatus.OK).json({
+                status: "OK",
+                project: data
+            });
+
+        } catch (error) {
+            return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+                status: "FAILED",
+                message: error.message
+            });
+        }
+    });
+
+    return router;
+};
+
+export default getProjectsRouter;
