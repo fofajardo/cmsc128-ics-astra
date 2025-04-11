74c27d93181d5cef928b7abf5fbadea52c3fb809
Nico De Castro <jtdecastro@up.edu.ph>
2025-04-11 23:48:27 +0800

    feat: implement get project donations route

--- api/routes/projects/get-projects.js
+++ api/routes/projects/get-projects.js
@@ -71,6 +71,43 @@ const getProjectsRouter = (supabase) => {
         }
     });
 
+    router.get("/:projectId/donations", async (req, res) => {
+        try {
+            const { projectId } = req.params;
+            const { page = 1, limit = 10 } = req.query;
+            const startIndex = (page - 1) * limit;
+            const endIndex = startIndex + Number(limit) - 1;
+
+            const { data, error } = await supabase
+                .from("donations")
+                .select(
+                    "id, alum_id, project_id, donation_date, reference_num, mode_of_payment, amount"
+                )
+                .eq("project_id", projectId)
+                .range(startIndex, endIndex);
+
+            if (error) {
+                return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+                    status: "FAILED",
+                    message: error.message
+                });
+            }
+
+            // console.log(data);
+
+            return res.status(httpStatus.OK).json({
+                status: "OK",
+                donations: data || [],
+            });
+
+        } catch (error) {
+            return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+                status: "FAILED",
+                message: error.message
+            });
+        }
+    });
+
     return router;
 };
 
