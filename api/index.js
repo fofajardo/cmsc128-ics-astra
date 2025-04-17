c0e67bcdb811e2eea99663c0a927170cd787cb02
johnpaulminoc <jnminoc@up.edu.ph>
2025-04-17 20:33:32 +0800

    chore:added admin folder' '

---
+++ api/index.js
@@ -0,0 +1,16 @@
+// /*
+//  * Base imports.
+//  */
+// import express from "express";
+
+// const kPort = 3001;
+
+// const gServer = express();
+
+// // Use appropriate parsers to access the request/response body directly.
+// gServer.use(express.json());
+// gServer.use(express.urlencoded({ extended: false }));
+
+// gServer.listen(kPort, function() {
+//     console.log(`Listening to port: ${kPort}`);
+// });
