28dcbd454597346fa299a6034f4c8f6c08a035ac
aguitezFRG <fpaguitez@up.edu.ph>
2025-04-18 00:00:33 +0800

    feat: implement initial routes for work_experiences entity

--- api/routes/loadRoutes.js
+++ api/routes/loadRoutes.js
@@ -1,9 +1,11 @@
 import usersRouter from "./usersRoutes.js";
 import alumniProfilesRouter from "./alumniProfilesRoutes.js";
+import workExperiencesRouter from "./workExperiencesRoutes.js";
 
 const registerRoutes = (app, supabase) => {
     app.use("/v1/users", usersRouter(supabase));
     app.use("/v1/alumniProfiles", alumniProfilesRouter(supabase));
+    app.use("/v1/work_experiences", workExperiencesRouter(supabase));
 }
 
 export default registerRoutes;
