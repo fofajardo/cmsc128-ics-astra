060b452ed96b0094013eca745f0e0199142941c3
Axel O. Balitaan <118553810+aobalitaan@users.noreply.github.com>
2025-04-18 13:48:24 +0800

    feat:specs from meeting, unfinished alumni search

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -5,13 +5,13 @@ import "../styles/globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="relative min-h-screen">
+      <body className="relative min-h-screen max-w-screen overflow-x-hidden">
         
         <div className="fixed top-0 left-0 w-full z-50">
           <AdminNavbar />
         </div>
 
-        <div className="pt-[100px] flex flex-col min-h-screen">
+        <div className="flex flex-col min-h-screen">
           <main className="flex-1">
             {children}
           </main>
