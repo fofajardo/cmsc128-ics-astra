22be455782e1327154ce7f1de4a13e92ae93a29a
MNAutriz <mgautriz@up.edu.ph>
2025-04-18 12:32:35 +0800

    Added the user navbar with avatar

--- app/(home)/layout.js
+++ app/(home)/layout.js
@@ -1,8 +1,8 @@
-import Navbar from '../components/Navbar';
+import NavbarUser from '../components/NavbarUser'; // Swap this line in
 import Footer from '../components/Footer'; 
 import '../styles/globals.css';
 import '../styles/styles.css';
-
+import Navbar from '../components/Navbar';
 
 export default function RootLayout({ children }) {
   return (
