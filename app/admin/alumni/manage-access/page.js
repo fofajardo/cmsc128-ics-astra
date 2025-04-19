8d878d15ee110076969e4009ae822392c66c7c48
riggs249 <rttomas@up.edu.ph>
2025-04-19 20:41:18 +0800

    feat: integrated AdminStatCard component to the manage-access page

--- app/admin/alumni/manage-access/page.js
+++ app/admin/alumni/manage-access/page.js
@@ -5,132 +5,8 @@ import {TableHeader, Table, PageTool} from '@/components/TableBuilder';
 import { users, alumniProfiles } from '@/components/DummyData'
 import SearchFilter from "admin/alumni/search/filter";
 import ToastNotification from '@/components/ToastNotification';
-import { Check } from "lucide-react";
-
-const alumList = [
-    {
-        id: 1,
-        image: "https://cdn-icons-png.flaticon.com/512/145/145974.png",
-        alumname: "Emma Johnson",
-        email: "emma.johnson@example.com",
-        graduationYear: 2015,
-        student_num: "2022-03814",
-        course: "BS Computer Science",
-        location: "New York, NY",
-        fieldOfWork: "Backend Development",
-        skills: ["Java", "Spring Boot", "REST APIs", "PostgreSQL"]
-    },
-    {
-        id: 2,
-        image: "https://cdn-icons-png.flaticon.com/512/145/145974.png",
-        alumname: "Liam Smith",
-        email: "liam.smith@example.com",
-        graduationYear: 2018,
-        student_num: "2021-03814",
-        course: "BS Civil Engineering",
-        location: "San Francisco, CA",
-        fieldOfWork: "Machine Learning Engineering",
-        skills: ["Python", "Scikit-learn", "Pandas"]
-    },
-    {
-        id: 3,
-        image: "https://cdn-icons-png.flaticon.com/512/145/145974.png",
-        alumname: "Olivia Brown",
-        email: "olivia.brown@example.com",
-        graduationYear: 2012,
-        student_num: "2020-03814",
-        course: "BS Education",
-        location: "Chicago, IL",
-        fieldOfWork: "Frontend Development",
-        skills: ["HTML", "CSS", "JavaScript", "Vue.js"]
-    },
-    {
-        id: 4,
-        image: "https://cdn-icons-png.flaticon.com/512/145/145974.png",
-        alumname: "Noah Davis",
-        email: "noah.davis@example.com",
-        graduationYear: 2020,
-        student_num: "2022-30214",
-        course: "BS Computer Science",
-        location: "Austin, TX",
-        fieldOfWork: "DevOps Engineering",
-        skills: ["Docker", "Kubernetes", "AWS", "CI/CD", "Terraform"]
-    },
-    {
-        id: 5,
-        image: "https://cdn-icons-png.flaticon.com/512/145/145974.png",
-        alumname: "Ava Wilson",
-        email: "ava.wilson@example.com",
-        graduationYear: 2017,
-        student_num: "2020-12314",
-        course: "BS Computer Science",
-        location: "Seattle, WA",
-        fieldOfWork: "Mobile App Development",
-        skills: ["Swift", "iOS", "Firebase"]
-    },
-    {
-        id: 6,
-        image: "https://cdn-icons-png.flaticon.com/512/145/145974.png",
-        alumname: "William Martinez",
-        email: "william.martinez@example.com",
-        graduationYear: 2014,
-        student_num: "2021-41237",
-        course: "BS Chemical Engineering",
-        location: "Miami, FL",
-        fieldOfWork: "Full Stack Development",
-        skills: ["Node.js", "React", "MongoDB", "GraphQL"]
-    },
-    {
-        id: 7,
-        image: "https://cdn-icons-png.flaticon.com/512/145/145974.png",
-        alumname: "Sophia Garcia",
-        email: "sophia.garcia@example.com",
-        graduationYear: 2016,
-        student_num: "2022-99632",
-        course: "BS Forestry",
-        location: "Denver, CO",
-        fieldOfWork: "Cloud Engineering",
-        skills: ["Azure", "Linux", "Networking", "Python"]
-    },
-    {
-        id: 8,
-        image: "https://cdn-icons-png.flaticon.com/512/145/145974.png",
-        alumname: "James Anderson",
-        email: "james.anderson@example.com",
-        graduationYear: 2013,
-        student_num: "2017-26934",
-        course: "BS Computer Science",
-        location: "Boston, MA",
-        fieldOfWork: "Security Engineering",
-        skills: ["Penetration Testing", "OWASP", "Metasploit"]
-    },
-    {
-        id: 9,
-        image: "https://cdn-icons-png.flaticon.com/512/145/145974.png",
-        alumname: "Isabella Thomas",
-        email: "isabella.thomas@example.com",
-        graduationYear: 2019,
-        student_num: "2015-04814",
-        course: "BS Industrial Engineering",
-        location: "Los Angeles, CA",
-        fieldOfWork: "AI Research",
-        skills: ["PyTorch", "Deep Learning", "NLP"]
-    },
-    {
-        id: 10,
-        image: "https://cdn-icons-png.flaticon.com/512/145/145974.png",
-        alumname: "Benjamin Lee",
-        email: "benjamin.lee@example.com",
-        graduationYear: 2011,
-        student_num: "2013-03825",
-        course: "BS Information Technology",
-        location: "Atlanta, GA",
-        fieldOfWork: "Database Administration",
-        skills: ["SQL", "Oracle", "Database Tuning", "Shell Scripting", "PL/SQL"]
-    }
-];
-    
-
+import { Check, GraduationCap } from "lucide-react";
+import AdminStatCard from "@/components/AdminStatCard";
 
 export default function AlumniAccess() {
     const [showFilter, setShowFilter] = useState(false);
@@ -166,11 +42,19 @@ export default function AlumniAccess() {
           <img
             src="/blue-bg.png"
             alt="Background"
-            className="h-64 w-full object-cover"
+            className="h-80 w-full object-cover"
           />
-          <div className="absolute inset-0 flex flex-col items-center justify-center text-astrawhite z-10">
-            <h1 className="font-h1 text-center">Manage Access</h1>
-            <p className="font-s text-center">The ever-growing UPLB-ICS Alumni Network</p>
+          <div className="absolute inset-2 flex flex-col items-center justify-evenly text-astrawhite z-20">
+            <div className="text-center pt-6">
+                <h1 className="font-h1">Manage Access</h1>
+                <p className="font-s">The ever-growing UPLB-ICS Alumni Network</p>
+            </div>
+            <div className="flex flex-row gap-3 pt-6"> 
+                <AdminStatCard title='Registered' value = {255} icon={<GraduationCap className='size-13 text-astrawhite/>' strokeWidth={1.5}/>} route={'/admin/alumni/search'}/>
+                <AdminStatCard title='Pending' value = {59} icon={<GraduationCap className='size-13 text-astrawhite/>' strokeWidth={1.5}/>} route={'/admin/alumni/manage-access'}/>
+                <AdminStatCard title='Approved' value = {179} icon={<GraduationCap className='size-13 text-astrawhite/>' strokeWidth={1.5}/>} route={'/admin/alumni/manage-access'}/>
+                <AdminStatCard title='Inactive' value = {12} icon={<GraduationCap className='size-13 text-astrawhite/>' strokeWidth={1.5}/>} route={'/admin/alumni/manage-access'}/>
+            </div>
           </div>
         </div>
   
@@ -316,3 +200,128 @@ function renderActions(id,name) {
         </>
     );
 }
+
+
+const alumList = [
+    {
+        id: 1,
+        image: "https://cdn-icons-png.flaticon.com/512/145/145974.png",
+        alumname: "Emma Johnson",
+        email: "emma.johnson@example.com",
+        graduationYear: 2015,
+        student_num: "2022-03814",
+        course: "BS Computer Science",
+        location: "New York, NY",
+        fieldOfWork: "Backend Development",
+        skills: ["Java", "Spring Boot", "REST APIs", "PostgreSQL"]
+    },
+    {
+        id: 2,
+        image: "https://cdn-icons-png.flaticon.com/512/145/145974.png",
+        alumname: "Liam Smith",
+        email: "liam.smith@example.com",
+        graduationYear: 2018,
+        student_num: "2021-03814",
+        course: "BS Civil Engineering",
+        location: "San Francisco, CA",
+        fieldOfWork: "Machine Learning Engineering",
+        skills: ["Python", "Scikit-learn", "Pandas"]
+    },
+    {
+        id: 3,
+        image: "https://cdn-icons-png.flaticon.com/512/145/145974.png",
+        alumname: "Olivia Brown",
+        email: "olivia.brown@example.com",
+        graduationYear: 2012,
+        student_num: "2020-03814",
+        course: "BS Education",
+        location: "Chicago, IL",
+        fieldOfWork: "Frontend Development",
+        skills: ["HTML", "CSS", "JavaScript", "Vue.js"]
+    },
+    {
+        id: 4,
+        image: "https://cdn-icons-png.flaticon.com/512/145/145974.png",
+        alumname: "Noah Davis",
+        email: "noah.davis@example.com",
+        graduationYear: 2020,
+        student_num: "2022-30214",
+        course: "BS Computer Science",
+        location: "Austin, TX",
+        fieldOfWork: "DevOps Engineering",
+        skills: ["Docker", "Kubernetes", "AWS", "CI/CD", "Terraform"]
+    },
+    {
+        id: 5,
+        image: "https://cdn-icons-png.flaticon.com/512/145/145974.png",
+        alumname: "Ava Wilson",
+        email: "ava.wilson@example.com",
+        graduationYear: 2017,
+        student_num: "2020-12314",
+        course: "BS Computer Science",
+        location: "Seattle, WA",
+        fieldOfWork: "Mobile App Development",
+        skills: ["Swift", "iOS", "Firebase"]
+    },
+    {
+        id: 6,
+        image: "https://cdn-icons-png.flaticon.com/512/145/145974.png",
+        alumname: "William Martinez",
+        email: "william.martinez@example.com",
+        graduationYear: 2014,
+        student_num: "2021-41237",
+        course: "BS Chemical Engineering",
+        location: "Miami, FL",
+        fieldOfWork: "Full Stack Development",
+        skills: ["Node.js", "React", "MongoDB", "GraphQL"]
+    },
+    {
+        id: 7,
+        image: "https://cdn-icons-png.flaticon.com/512/145/145974.png",
+        alumname: "Sophia Garcia",
+        email: "sophia.garcia@example.com",
+        graduationYear: 2016,
+        student_num: "2022-99632",
+        course: "BS Forestry",
+        location: "Denver, CO",
+        fieldOfWork: "Cloud Engineering",
+        skills: ["Azure", "Linux", "Networking", "Python"]
+    },
+    {
+        id: 8,
+        image: "https://cdn-icons-png.flaticon.com/512/145/145974.png",
+        alumname: "James Anderson",
+        email: "james.anderson@example.com",
+        graduationYear: 2013,
+        student_num: "2017-26934",
+        course: "BS Computer Science",
+        location: "Boston, MA",
+        fieldOfWork: "Security Engineering",
+        skills: ["Penetration Testing", "OWASP", "Metasploit"]
+    },
+    {
+        id: 9,
+        image: "https://cdn-icons-png.flaticon.com/512/145/145974.png",
+        alumname: "Isabella Thomas",
+        email: "isabella.thomas@example.com",
+        graduationYear: 2019,
+        student_num: "2015-04814",
+        course: "BS Industrial Engineering",
+        location: "Los Angeles, CA",
+        fieldOfWork: "AI Research",
+        skills: ["PyTorch", "Deep Learning", "NLP"]
+    },
+    {
+        id: 10,
+        image: "https://cdn-icons-png.flaticon.com/512/145/145974.png",
+        alumname: "Benjamin Lee",
+        email: "benjamin.lee@example.com",
+        graduationYear: 2011,
+        student_num: "2013-03825",
+        course: "BS Information Technology",
+        location: "Atlanta, GA",
+        fieldOfWork: "Database Administration",
+        skills: ["SQL", "Oracle", "Database Tuning", "Shell Scripting", "PL/SQL"]
+    }
+];
+    
