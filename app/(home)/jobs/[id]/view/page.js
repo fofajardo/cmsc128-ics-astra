dfba7dd52759e13ed336bd4a4f15b1c150229299
juli-es <jinamata@up.edu.ph>
2025-04-19 12:36:32 +0800

    feat: view job details alumni view

---
+++ app/(home)/jobs/[id]/view/page.js
@@ -0,0 +1,49 @@
+import BigJobCard from "../../../../components/jobs/view/bigJobCard";
+import SmallJobCard from "../../../../components/jobs/view/smallJobCard";
+import { Icon } from "@iconify/react";
+
+export default function JobsPage() {
+    const job = {
+        salary:70000, 
+        title:"Google Cloud Engineer", 
+        company:"Google Philippines", 
+        location:"38th Floor, Seven NEO, Building 5th Ave, Manila, 1016 Metro Manila", 
+        expiryDate: "June 16, 2025, 11:59 PM", 
+        employmentType: "Full-Time", 
+        workArrangement: "Onsite",
+        details: 
+        `As a Cloud Engineer at Google Cloud, you'll design,  build, and optimize scalable cloud solutions that help businesses  transform and innovate. You'll work with cutting-edge technologies like Kubernetes, BigQuery, and AI/ML tools to architect secure, high-performance infrastructure. Partnering with customers and internal  teams, you’ll solve complex technical challenges and drive cloud adoption worldwide.
+
+        Key Responsibilities:
+        Design and deploy secure, scalable cloud architectures on Google Cloud Platform (GCP).
+        Automate infrastructure using Terraform, Ansible, or other IaC tools.
+        Troubleshoot performance issues and optimize cost-efficiency.
+        Collaborate with customers and internal teams to implement best practices.
+
+        Requirements:
+        Bachelor’s degree in Computer Science, Engineering, or related field (or equivalent experience).
+        Hands-on experience with GCP, AWS, or Azure (certifications a plus).
+        Proficiency in Python, Go, or scripting languages.
+        Strong knowledge of networking, security, and DevOps practices.
+
+        Why Join? Work on the forefront of cloud innovation, shape the future of enterprise tech, and grow with a global leader in  cloud computing.
+
+        Interested? Explore openings at careers.google.com. 🚀`
+    }
+
+    return (
+    <div className="mt-[80px] py-8 bg-astratintedwhite w-full flex flex-col items-center">
+        
+        <div className="flex gap-2 self-start ml-15 mb-5">
+            <Icon icon="lucide:arrow-left" width="29" height="29" className="shrink-0"/>
+            <button className="text-astrablack font-semibold text-xl">Back</button>
+        </div>
+
+        <BigJobCard {...job}/>
+
+        <div className="h-11"/>
+       
+        <SmallJobCard {...job}/>
+    </div>
+  )}
+  
