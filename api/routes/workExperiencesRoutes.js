cf475a8e3473116ab81b53650d28067ff6cb926c
aguitezFRG <fpaguitez@up.edu.ph>
2025-04-19 22:18:12 +0800

    chore: replaced double quotes with single quotes for consistency

--- api/routes/workExperiencesRoutes.js
+++ api/routes/workExperiencesRoutes.js
@@ -1,15 +1,15 @@
-import express from "express";
-import workExperiencesController from "../controllers/workExperiencesController.js";
+import express from 'express';
+import workExperiencesController from '../controllers/workExperiencesController.js';
 
 const workExperiencesRouter = (supabase) => {
     const router = express.Router();
 
-    router.get("/", workExperiencesController.getWorkExperiences(supabase));
-    router.get("/:workExperienceId", workExperiencesController.getWorkExperienceById(supabase));
-    router.get("/alum/:alumId", workExperiencesController.getWorkExperiencesByAlumId(supabase));
-    router.post("/", workExperiencesController.createWorkExperience(supabase));
-    router.put("/:workExperienceId", workExperiencesController.updateWorkExperience(supabase));
-    router.delete("/:workExperienceId", workExperiencesController.deleteWorkExperience(supabase));
+    router.get('/', workExperiencesController.getWorkExperiences(supabase));
+    router.get('/:workExperienceId', workExperiencesController.getWorkExperienceById(supabase));
+    router.get('/alum/:alumId', workExperiencesController.getWorkExperiencesByAlumId(supabase));
+    router.post('/', workExperiencesController.createWorkExperience(supabase));
+    router.put('/:workExperienceId', workExperiencesController.updateWorkExperience(supabase));
+    router.delete('/:workExperienceId', workExperiencesController.deleteWorkExperience(supabase));
 
     return router;
 }
