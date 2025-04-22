41c596db36a43969ffd0967bee513812fcefef88
Axel O. Balitaan <118553810+aobalitaan@users.noreply.github.com>
2025-04-22 18:10:43 +0800

    style: updated admin layout.js

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -2,13 +2,22 @@ import NavbarAdmin from '@/components/NavbarAdmin';
 import Footer from '@/components/Footer';
 import "@/styles/globals.css";
 
-export default function AdminLayout({ children }) {
-    return (
-        <html lang="en">
-            <body>
-                <NavbarAdmin />
-                <main className="pt-20">{children}</main>
-            </body>
-        </html>
-    );
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="relative min-h-screen max-w-screen overflow-x-hidden">
+        
+        <div className="fixed top-0 left-0 w-full z-50">
+          <NavbarAdmin />
+        </div>
+
+        <div className="flex flex-col min-h-screen pt-20">
+          <main className="flex-1">
+            {children}
+          </main>
+          <Footer />
+        </div>
+      </body>
+    </html>
+  );
 }
