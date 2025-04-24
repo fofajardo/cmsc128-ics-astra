419f23983a5d6cad4fe34c268c6d66fdcea07019
camanzanido <camanzanido.up.edu.ph>
2025-04-24 17:12:08 +0800

    feat: add filtering for events

--- api/controllers/eventsController.js
+++ api/controllers/eventsController.js
@@ -4,8 +4,8 @@ import { isValidUUID, isValidDate } from "../utils/validators.js";
 
 const getEvents = (supabase) => async (req, res) => {
     try {
-        const { page = 1, limit = 10 } = req.query;
-        const { data, error } = await eventsService.fetchEvents(supabase, page, limit);
+        const filters = req.query;
+        const { data, error } = await eventsService.fetchEvents(supabase, filters);
 
         if (error) {
             return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
