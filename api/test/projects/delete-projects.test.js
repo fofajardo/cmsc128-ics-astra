3118c112aac2d72853a93668eb2e7bc9c2c8dcd8
Nico De Castro <jtdecastro@up.edu.ph>
2025-04-25 00:26:33 +0800

    test: implement sign in/out and use single agent in project tests

--- api/test/projects/delete-projects.test.js
+++ api/test/projects/delete-projects.test.js
@@ -2,31 +2,35 @@ import request from "supertest";
 import { expect } from "chai";
 import app from "../../index.js";
 import httpStatus from "http-status-codes";
+import {TestSignIn, TestSignOut, TestUsers} from "../auth/auth.common.js";
+const gAgent = request.agent(app);
 
 describe('Projects API Tests', function () {
+    before(() => TestSignIn(gAgent, TestUsers.moderator));
+
     const projectId = '389517e7-4a0b-4c96-84f9-3a7080186892'; // Actual projectId
 
-     // Create a new dummy data for deletion test
-     before(async function () {
-        const res = await request(app)
-            .post(`/v1/projects`)
-            .send({
-                project_id: projectId,
-                status: 0,
-                due_date: new Date('2025-04-01'),
-                date_completed: null,
-                goal_amount: 100000,
-                donation_link: 'astra.com/amis-server-upgrade',
-            });
-        if (res.body.status === 'CREATED')
-            console.log('Successfully created dummy project');
-        else
-            console.log('Failed to create dummy project');
+    // Create a new dummy data for deletion test
+    before(async function () {
+    const res = await gAgent
+        .post(`/v1/projects`)
+        .send({
+            project_id: projectId,
+            status: 0,
+            due_date: new Date('2025-04-01'),
+            date_completed: null,
+            goal_amount: 100000,
+            donation_link: 'astra.com/amis-server-upgrade',
+        });
+    if (res.body.status === 'CREATED')
+        console.log('Successfully created dummy project');
+    else
+        console.log('Failed to create dummy project');
     });
 
     describe('DELETE /v1/projects/:projectId', function () {
         it('should return 200, a status DELETED, and a message', async function () {
-            const res = await request(app)
+            const res = await gAgent
                 .delete(`/v1/projects/${projectId}`)
 
             // console.log(res.body);
@@ -38,13 +42,13 @@ describe('Projects API Tests', function () {
         });
 
         it('should return 404, status FAILED, and a message when fetching the deleted project', async function () {
-            const res = await request(app).post(`/v1/projects/${projectId}`);
+            const res = await gAgent.post(`/v1/projects/${projectId}`);
 
             expect(res.statusCode).to.equal(httpStatus.NOT_FOUND);
         });
 
         it('should return 400 when projectId is not a valid UUID', async function () {
-            const res = await request(app)
+            const res = await gAgent
                 .delete('/v1/projects/not-a-valid-id');
 
             expect(res.status).to.equal(httpStatus.BAD_REQUEST);
@@ -56,7 +60,7 @@ describe('Projects API Tests', function () {
 
         it('should return 404, a status FAILED, and a message when deleting a non-existing project', async function () {
             const nonExistentId = '389517e7-4a0b-4c96-84f9-3a7080186892';   // non-existing id
-            const res = await request(app)
+            const res = await gAgent
                 .delete(`/v1/projects/${nonExistentId}`);
 
             expect(res.status).to.equal(httpStatus.NOT_FOUND);
@@ -68,10 +72,10 @@ describe('Projects API Tests', function () {
 
         it('should return 404 when trying to delete already-deleted project', async function () {
             // First deletion
-            await request(app).delete(`/v1/projects/${projectId}`);
+            await gAgent.delete(`/v1/projects/${projectId}`);
 
             // Second deletion attempt
-            const res = await request(app).delete(`/v1/projects/${projectId}`);
+            const res = await gAgent.delete(`/v1/projects/${projectId}`);
 
             expect(res.status).to.equal(httpStatus.NOT_FOUND);
             expect(res.body).to.be.an('object');
@@ -80,4 +84,6 @@ describe('Projects API Tests', function () {
             expect(res.body).to.have.property('message').that.is.a('string');
         });
     });
+
+    after(() => TestSignOut(gAgent));
 });
--- api/test/projects/post-projects.test.js
+++ api/test/projects/post-projects.test.js
@@ -2,23 +2,19 @@ import request from 'supertest';
 import { expect } from 'chai';
 import app from '../../index.js';
 import httpStatus from 'http-status-codes';
+import {TestSignIn, TestSignOut, TestUsers} from "../auth/auth.common.js";
+const gAgent = request.agent(app);
 
 describe('Projects API Tests', function () {
-    describe('POST /v1/projects', function () {
-        const contentId = '318c8aab-0564-42f8-aba6-5785d0e66288';
+    before(() => TestSignIn(gAgent, TestUsers.moderator));
 
-        after(async function () {
-            const res = await request(app)
-                .delete(`/v1/projects/${contentId}`);
-            if (res.body.status === 'DELETED') {
-                console.log('Successfully deleted dummy project');
-            } else
-                console.log('Failed to delete dummy project');
-        });
+    const contentId = '318c8aab-0564-42f8-aba6-5785d0e66288';
+
+    describe('POST /v1/projects', function () {
 
         // Test case to verify that the API returns 400 if required fields are missing
         it('should return 400, status FAILED, and a message when required fields are missing', async function () {
-            const res = await request(app)
+            const res = await gAgent
                 .post(`/v1/projects`)
                 .send({});
 
@@ -33,7 +29,7 @@ describe('Projects API Tests', function () {
 
         // Test case to verify that the API returns 400 if invalid status
         it('should return 400, status FAILED, and a message when status is invalid', async function () {
-            const res = await request(app)
+            const res = await gAgent
                 .post(`/v1/projects`)
                 .send({
                     project_id: contentId,
@@ -55,7 +51,7 @@ describe('Projects API Tests', function () {
 
         // Test case to verify that the API returns 400 if invalid date
         it('should return 400, status FAILED, and a message when date is invalid', async function () {
-            const res = await request(app)
+            const res = await gAgent
                 .post(`/v1/projects`)
                 .send({
                     project_id: contentId,
@@ -77,7 +73,7 @@ describe('Projects API Tests', function () {
 
         // // Test case to verify that the project is created successfully
         it('should return 201, status CREATED, a message, and an id', async function () {
-            const res = await request(app)
+            const res = await gAgent
                 .post(`/v1/projects`)
                 .send({
                     project_id: contentId,
@@ -101,7 +97,7 @@ describe('Projects API Tests', function () {
         // Test case to verify that the API returns 400 if invalid contentId
         it('should return 400, status FAILED, and a message when projectId is invalid', async function () {
             const invalidContentId = '00000000-0000-0000-0000-000000000000'; // Invalid content ID
-            const res = await request(app)
+            const res = await gAgent
                 .post(`/v1/projects`)
                 .send({
                     project_id: invalidContentId,
@@ -124,7 +120,7 @@ describe('Projects API Tests', function () {
         // Test case to verify that the API returns 404 if the contentId does not exist in the system
         it('should return 404, status FAILED, and a message when contentId does not exist', async function () {
             const nonExistentId = '389517e7-4a0b-4c96-84f9-3a7080186893'; // Non-existing content ID
-            const res = await request(app)
+            const res = await gAgent
                 .post(`/v1/projects`)
                 .send({
                     project_id: nonExistentId,
@@ -142,7 +138,7 @@ describe('Projects API Tests', function () {
 
         // Test case to verify that the API returns 409 if the contentId has duplicate
         it('should return 409, status FAILED, and a message when contentId has duplicate', async function () {
-            const res = await request(app)
+            const res = await gAgent
                 .post(`/v1/projects`)
                 .send({
                     project_id: contentId,
@@ -162,4 +158,15 @@ describe('Projects API Tests', function () {
             expect(res.body).to.have.property('message').that.is.a('string');
         });
     });
+
+    after(async function () {
+        const res = await gAgent
+            .delete(`/v1/projects/${contentId}`);
+        if (res.body.status === 'DELETED') {
+            console.log('Successfully deleted dummy project');
+        } else
+            console.log('Failed to delete dummy project');
+    });
+
+    after(() => TestSignOut(gAgent));
 });
