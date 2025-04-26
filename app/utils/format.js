06eac26856cb12acd7cdc2684dce8267704f3575
aguitezFRG <fpaguitez@up.edu.ph>
2025-04-27 03:06:55 +0800

    util: added utility functions for formatting date and salary

--- app/utils/format.js
+++ app/utils/format.js
@@ -5,3 +5,19 @@ export function capitalizeName(name) {
         .map(word => word.charAt(0).toUpperCase() + word.slice(1))
         .join(' ');
 }
+
+export function formatDate(date) {
+    const options = { 
+        year: 'numeric', 
+        month: 'long', 
+        day: 'numeric'
+    };
+    return new Date(date).toLocaleDateString('en-US', options);
+}
+
+export function formatSalary(salary) {
+    if (salary === null) {
+        return 'N/A';
+    }
+    return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'PHP' }).format(salary);
+}
