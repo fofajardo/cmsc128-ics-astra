3c0c372aa2dd8af608c046ecbe01bf0a3e3c146f
fofajardo <fofajardo@up.edu.ph>
2025-04-27 02:58:02 +0800

    chore: drop express session.

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
 import env from "dotenv";
 import express from "express";
-import session from "express-session";
 import httpStatus from "http-status-codes";
 import registerRoutes from "./routes/loadRoutes.js";
 import {InferAbility} from "./middleware/inferAbility.js";
@@ -19,14 +18,6 @@ gServer.use(express.urlencoded({ extended: false }));
 gServer.get("/", (req, res) => {
     res.status(httpStatus.OK).json({ message: "API is working!" });
 });
-
-// Set up session handling to use .
-gServer.use(session({
-    resave: false,
-    saveUninitialized: false,
-    secret: "047afa6b-829a-432a-b962-e7a4e9fe7d9d",
-    // FIXME: sessions are not persisted.
-}));
 
 gServer.use(InferAbility);
 gServer.use(BuildSupabaseClient);
