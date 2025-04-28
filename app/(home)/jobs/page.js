b8f11f660faed561e21ec9a470d89df0cc3a6b43
juli-es <jinamata@up.edu.ph>
2025-04-28 16:47:27 +0800

    ui: wip show expired toggle

--- app/(home)/jobs/page.js
+++ app/(home)/jobs/page.js
@@ -6,8 +6,9 @@ import JobEditCard from "../../components/jobs/jobEditCard";
 import SearchBar from "../../components/jobs/search";
 import Filter from "../../components/jobs/filters";
 import Image from "next/image";
-import {dummyJobs, dummyMyJobs, filters} from './dummy'
+import {dummyJobs, dummyMyJobs} from './dummy'
 import { useState, useEffect } from 'react';
+import { Eye, EyeOff } from "lucide-react";
 
 export default function JobsPage() {
     const [jobs, setJobs] = useState(dummyJobs);
@@ -15,6 +16,8 @@ export default function JobsPage() {
     const [jobCards, setJobCards] = useState(6); // limit / no. of cards to show
     const [myJobCards, setMyJobCards] = useState(6); // limit / no. of cards owned to show
     const CARDS_PER_CLICK = 6;
+
+    const [showExpired, setExpired] = useState(false);
 
     useEffect(() => {
     // fetch jobs logic here whenever See More component is clicked
@@ -58,7 +61,14 @@ export default function JobsPage() {
       : 
       <>
         <div className="grid grid-cols-[351px] lg:grid-cols-[351px_351px_351px] md:grid-cols-[351px_351px] gap-5 justify-items-center justify-center mx-30">
-          <h1 className="text-astrablack font-bold text-2xl ml-2 lg:col-span-3 md:col-span-2 justify-self-start">Manage My Job Postings</h1>
+          <div className="flex justify-between lg:col-span-3 md:col-span-2 justify-self-start items-center">
+            <h1 className="text-astrablack font-bold text-2xl ml-2">Manage My Job Postings</h1>
+            {/* <button onClick={() => setExpired(!showExpired)} name={"recent"} className={`ml-3 ${showExpired ? 'bg-[var(--color-astraprimary)] text-[var(--color-astrawhite)]' : 'text-[var(--color-astrablack)] bg-[var(--color-astrawhite)]'} !cursor-pointer flex items-center justify-between outline outline-transparent border-1 border-astraprimary font-normal px-4 h-[45px] w-30 rounded-lg placeholder:text-astradarkgray -translate-y-0 hover:-translate-y-1 hover:shadow-[0_2px_4px_rgba(0,0,0,0.25)] hover:scale-none`} tabIndex={0}>
+                <p>Expired</p>
+                {showExpired ? <Eye className={`text-[var(--color-astrawhite)]`} size={25} strokeWidth={2}/>
+                : <EyeOff className={'text-[var(--color-astraprimary)]'} size={25} strokeWidth={2}/>}
+            </button> */}
+          </div>
               {myJobs.slice(0, myJobCards).map((job) => {
               return (
                 <JobEditCard key={job.job_id} job={job}/>
