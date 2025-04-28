1b0db3d8868d410ee0c7f08b84400a7d4da72b01
juli-es <jinamata@up.edu.ph>
2025-04-28 23:07:07 +0800

    feat: homepage astra about

--- app/(home)/page.js
+++ app/(home)/page.js
@@ -8,7 +8,7 @@ import { FileText } from 'lucide-react';
 export default function Page() {
   
     return (
-      <div className = "landing-page">
+      <div className = "relative bg-astratintedwhite">
         <title>ICS-ASTRA</title>
         <link rel = "icon" href = "/astra-logo.png" />
 
@@ -16,12 +16,21 @@ export default function Page() {
           <Slideshow />
         </div>
 
-        <div className = "bg-astratintedwhite">
-          <div style = {{display: 'flex', justifyContent: 'center', padding: '50px'}}>
-            <img src = '/astra-logo.png'/>
+        {/* ICS-ASTRA Defined */}
+        <div>
+          <div className='flex flex-col items-center p-[100px] gap-y-15'>
+            <Image src="/astra-logo.png" width={300} height={300} alt='loc' className="shrink-0"/>
+            <p className='font-medium text-3xl text-astrablack text-center w-2/3'>
+              <span className='font-black text-4xl text-astraprimary'>ICS-ASTRA {" "}</span> 
+              is a system catered towards the alumni of the Institute of Computer Science (ICS) of the University of the Philippines Los Baños (UPLB). Its purpose is to improve and maintain the connections between ICS and its alumni that it had produced over the years.  
+            </p>
           </div>
         </div>
 
+      <div className='bg-[#E5EAF8] w-full p-20 flex items-center justify-center gap-20'>
+        <Image src="/astra-logo.png" width={450} height={425} alt='loc' className="shrink-0"/>
+        <p className='w-1/3 text-right font-medium text-3xl text-astrablack'> Stay connected with the latest updates from Astra-ICS. From important news to exciting announcements, find out what’s happening, what’s new, and what’s coming next — all right here  </p>
+      </div>
         
       </div>
     );
