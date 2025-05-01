96d61a90f268230e466daffaee48f921b1c452b0
fofajardo <fofajardo@up.edu.ph>
2025-05-01 10:54:42 +0800

    fix: add missing root layout and wrap children with user provider.

---
+++ app/layout.js
@@ -0,0 +1,29 @@
+import { SignedInUserFetcher, SignedInUserProvider } from "./components/UserContext";
+
+export const metadata = {
+  title: {
+    template: "%s - ICS- ASTRA",
+    default: "ICS-ASTRA",
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/favicon.png" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+        />
+      </head>
+      <body>
+        <SignedInUserProvider>
+          <SignedInUserFetcher />
+          {children}
+        </SignedInUserProvider>
+      </body>
+    </html>
+  );
+}
+
