2252afd9a9f82e7b4931db2b4fb47fca3a308323
fofajardo <fofajardo@up.edu.ph>
2025-05-04 17:50:18 +0800

    fix: handle sign out errors gracefully.

--- app/(auth)/sign-out/page.jsx
+++ app/(auth)/sign-out/page.jsx
@@ -5,14 +5,22 @@ import {useEffect} from "react";
 import axios from "axios";
 import {clientRoutes} from "../../../common/routes.js";
 import {useRouter} from "next/navigation";
+import {useRefetchUser, useSignedInUser} from "@/components/UserContext.jsx";
 
 export default function SignOut() {
   const router = useRouter();
+  const userContext = useSignedInUser();
 
   const handleSignOut = function() {
     axios.post(clientRoutes.auth.signOut(),
     ).then(function() {
-      router.push("/");
+      // Do nothing with the result.
+    }).catch(function() {
+      // Ignore errors.
+    }).finally(function() {
+      useRefetchUser(userContext).then(function () {
+        return router.push("/");
+      });
     });
   };
 
