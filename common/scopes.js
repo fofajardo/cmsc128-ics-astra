60962e7e0069872b98f09e5d876370cba708bfd6
aguitezFRG <fpaguitez@up.edu.ph>
2025-05-11 23:32:39 +0800

    feat/chore: migrated PhotoType enums, and added EventStatus enums

--- common/scopes.js
+++ common/scopes.js
@@ -63,4 +63,70 @@ const Actions = Object.freeze({
   MANAGE: "manage",
 });
 
-export { RoleName, Subjects, Actions };
+const PhotoType = Object.freeze({
+  PROFILE_PIC: 0,
+  PROOF_OF_PAYMENT: 1,
+  PROOF_OF_GRADUATION: 2,
+  EVENT_PIC: 3,
+  JOB_PIC: 4,
+  PROJECT_PIC: 5,
+  POSTS_PIC: 6,
+  isDefined: function(aValue) {
+    if (typeof aValue === "string" || aValue instanceof String) {
+      const value = aValue.toLowerCase().trim();
+      switch (value) {
+      case PhotoType.PROFILE_PIC:
+      case PhotoType.PROOF_OF_PAYMENT:
+      case PhotoType.PROOF_OF_GRADUATION:
+      case PhotoType.EVENT_PIC:
+      case PhotoType.JOB_PIC:
+      case PhotoType.PROJECT_PIC:
+      case PhotoType.POSTS_PIC:
+        return true;
+      default:
+        break;
+      }
+    }
+    return false;
+  },
+  parse: function(aValue) {
+    if (!this.isDefined(aValue)) {
+      return null;
+    }
+    return parseInt(aValue);
+  },
+});
+
+const EventStatus = Object.freeze({
+  OPEN: "open",
+  CLOSED: "closed",
+  CANCELLED: "cancelled",
+  isDefined: function(aValue) {
+    if (typeof aValue === "string" || aValue instanceof String) {
+      const value = aValue.toLowerCase().trim();
+      switch (value) {
+      case EventStatus.OPEN:
+      case EventStatus.CLOSED:
+      case EventStatus.CANCELLED:
+        return true;
+      default:
+        break;
+      }
+    }
+    return false;
+  },
+  parse: function(aValue) {
+    if (!this.isDefined(aValue)) {
+      return null;
+    }
+    return parseInt(aValue);
+  },
+});
+
+export {
+  RoleName,
+  Subjects,
+  Actions,
+  PhotoType,
+  EventStatus
+};
