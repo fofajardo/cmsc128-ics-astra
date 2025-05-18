61f1e037f336fe4569c9a88325d499b3f8e10bde
fofajardo <fofajardo@up.edu.ph>
2025-05-18 16:44:11 +0800

    feat: insert a loading overlay to dialog.

--- app/components/ui/dialog.jsx
+++ app/components/ui/dialog.jsx
@@ -5,6 +5,7 @@ import * as DialogPrimitive from "@radix-ui/react-dialog";
 import { XIcon } from "lucide-react";
 
 import { cn } from "@/lib/utils";
+import LoadingOverlay from "@/components/LoadingOverlay.jsx";
 
 function Dialog({
   ...props
@@ -48,8 +49,15 @@ function DialogOverlay({
 function DialogContent({
   className,
   children,
+  loading,
   ...props
 }) {
+  const preventClose = function(e) {
+    if (loading) {
+      e.preventDefault();
+    }
+  };
+
   return (
     <DialogPortal data-slot="dialog-portal">
       <DialogOverlay />
@@ -59,7 +67,10 @@ function DialogContent({
           "bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg p-6 shadow-lg duration-200 sm:max-w-lg",
           className
         )}
+        onEscapeKeyDown={preventClose}
+        onPointerDownOutside={preventClose}
         {...props}>
+        <LoadingOverlay loading={loading} className="rounded-lg"/>
         {children}
         <DialogPrimitive.Close
           className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4">
