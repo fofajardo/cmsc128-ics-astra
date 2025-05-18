aba31c56673589ac72958bfe3d1b5d6a722d21a8
fofajardo <fofajardo@up.edu.ph>
2025-05-18 15:24:24 +0800

    chore: add sonner.

--- app/layout.jsx
+++ app/layout.jsx
@@ -24,6 +24,7 @@ export default function RootLayout({ children }) {
           <SignedInUserFetcher />
           <RootLoadingOverlay />
           {children}
+          <Toaster />
         </SignedInUserProvider>
       </body>
     </html>
