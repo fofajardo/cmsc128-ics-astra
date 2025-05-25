66ea02bd696f5fcb2d21b55f503c4698eafa4839
fofajardo <fofajardo@up.edu.ph>
2025-05-26 06:44:11 +0800

    chore: rename to jsx.

--- app/profile/layout.jsx
+++ app/profile/layout.jsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import "../styles/styles.css";
 import {Header} from "../components/Header.jsx";
 import {UserFetcher, UserProvider} from "@/components/UserContext.jsx";
-import ProfileInterstitial from "@/components/ProfileInterstitial.js";
+import ProfileInterstitial from "@/components/ProfileInterstitial.jsx";
 
 export default function RootLayout({ children }) {
   return (
