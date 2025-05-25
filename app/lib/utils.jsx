bc10680cb8354f8e637d4545de1dd0b9a5cee34d
fofajardo <fofajardo@up.edu.ph>
2025-05-25 14:47:51 +0800

    feat: add utility function for formatting date as Month Year.

--- app/lib/utils.jsx
+++ app/lib/utils.jsx
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export function formatMonthYear(input) {
+  const date = (typeof input === "string") ? new Date(input) : input;
+  if (!(date instanceof Date) || isNaN(date)) {
+    return "Invalid date input";
+  }
+  const options = { year: "numeric", month: "long" };
+  return date.toLocaleDateString("en-US", options);
+}
